59d78dee83252a4e363f6138f06bff181a2e6cc7
Tapio Vuorinen <delic@iki.fi>
2012-08-25 13:16:48 +0300

    added list ordering

--- caman.pl
+++ caman.pl
@@ -88,19 +88,19 @@ if ($command) {
 			$table = Text::Table->new("ID", "Name", "Description", "Notes");
 		    }
 		    case "device" {
-			$sth = $dbh->prepare("SELECT device.id, device_type.name, device.name, rack.name, device.description, device.notes FROM device INNER JOIN device_type ON device.device_type_id=device_type.id INNER JOIN rack ON device.rack_id=rack.id");
+			$sth = $dbh->prepare("SELECT device.id, device_type.name, device.name, rack.name, device.description, device.notes FROM device INNER JOIN device_type ON device.device_type_id=device_type.id INNER JOIN rack ON device.rack_id=rack.id ORDER BY rack.name");
 			$table = Text::Table->new("ID", "Type", "Name", "Rack", "Description", "Notes");
 		    }
 		    case "rack" {
-			$sth = $dbh->prepare("SELECT rack.id, rack.name, rack.description, room.name, rack.notes FROM rack INNER JOIN room ON rack.room_id=room.id");
+			$sth = $dbh->prepare("SELECT rack.id, rack.name, rack.description, room.name, rack.notes FROM rack INNER JOIN room ON rack.room_id=room.id ORDER BY room.name");
 			$table = Text::Table->new("ID", "Name", "Description", "Room", "Notes");
 		    }
 		    case "interface" {
-			$sth = $dbh->prepare("SELECT interface.id, interface.name, interface_type.name, device.name, interface.notes FROM interface INNER JOIN interface_type ON interface.interface_type_id=interface_type.id INNER JOIN device ON interface.device_id=device.id");
+			$sth = $dbh->prepare("SELECT interface.id, interface.name, interface_type.name, device.name, interface.notes FROM interface INNER JOIN interface_type ON interface.interface_type_id=interface_type.id INNER JOIN device ON interface.device_id=device.id ORDER BY device.name ASC, interface.name ASC");
 			$table = Text::Table->new("ID", "Name", "Type", "Device", "Notes");
 		    }
 		    case "connection" {
-			$sth = $dbh->prepare("SELECT connection.id, d1.name || '.' || i1.name, d2.name || '.' || i2.name, connection_type.name FROM connection, device as d1, interface as i1, device as d2, interface as i2, connection_type WHERE i1.id=connection.from_interface_id AND i1.device_id=d1.id AND i2.id=connection.to_interface_id AND i2.device_id=d2.id AND connection.connection_type_id=connection_type.id");
+			$sth = $dbh->prepare("SELECT connection.id, d1.name || '.' || i1.name, d2.name || '.' || i2.name, connection_type.name FROM connection, device as d1, interface as i1, device as d2, interface as i2, connection_type WHERE i1.id=connection.from_interface_id AND i1.device_id=d1.id AND i2.id=connection.to_interface_id AND i2.device_id=d2.id AND connection.connection_type_id=connection_type.id ORDER BY connection_type.name ASC, d1.name ASC");
 			$table = Text::Table->new("ID", "From", "To", "Type");
 		    }
 		    else { print_help_list(); }
